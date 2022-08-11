5049579d0316332cf24b7ec2eddeb2e5babe0ae1
Atul-Panchal <panchalatul7011@gmail.com>
2022-08-11 20:58:48 +0530

    update

--- src/Pages/StartTradeOne.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-function StartTradeOne() {
-  return <div></div>;
-}
-
-export default StartTradeOne;
--- src/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-
-import authReducer from './authReducer';
-import loadingReducer from './loadingReducer';
-
-export default combineReducers({
-  auth: authReducer,
-  loading: loadingReducer,
-});
